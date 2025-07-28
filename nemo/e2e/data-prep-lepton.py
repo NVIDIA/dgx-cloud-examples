633fe917435f7f61135825e11e9c0848f9899dd5
Robert Clark <roclark@nvidia.com>
2025-07-28 09:17:01 -0700

    Update NeMo FW e2e guide to Nemotron-H
    Updated the NeMo Framework end-to-end guide to use Nemotron-H as the
    backbone for the model architecture for improved inference throughput,
    ease of deployment, faster training times, and higher downstream
    accuracy compared to the Llama 3.1 8B example originally used.
    
    Signed-Off-By: Robert Clark <roclark@nvidia.com>

--- nemo/e2e/data-prep-lepton.py
+++ nemo/e2e/data-prep-lepton.py
@@ -15,22 +15,21 @@
 
 import nemo_run as run
 
-from data_prep.download import download
 from data_prep.extract import extract
-from data_prep.preprocess import preprocess
+from data_prep.preprocess import prepare
 
 def lepton_executor(nodes: int = 1, devices: int = 1) -> run.LeptonExecutor:
     mounts = [
         {
-            "path": "/",  # Directory to mount from the remote filesystem
-            "mount_path": "/nemo-workspace"  # Where to mount the directory in pods
+            "path": "/nemo-workspace",  # Directory to mount from the remote filesystem
+            "mount_path": "/nemo-workspace",  # Where to mount the directory in pods
             "from": "local:nfs"  # (Optional) Which remote storage resource to mount
         }
     ]
 
     return run.LeptonExecutor(
-        resource_shape="gpu.a100-80gb",  # Replace with the resource shape for the node group
-        container_image="nvcr.io/nvidia/nemo:25.02",  # Which container to deploy
+        resource_shape=f"gpu.{devices}xh100-80gb",  # Replace with the resource shape for the node group
+        container_image="nvcr.io/nvidia/nemo:25.04",  # Which container to deploy
         nemo_run_dir="/nemo-workspace/nemo-run",  # Specify the NeMo-Run directory to copy experiments to in the remote filesystem
         mounts=mounts,  # Which directories to mount from the remote filesystem
         node_group="xxxxx",  # Replace with the name of the node group available in the cluster
@@ -47,19 +46,22 @@ def lepton_executor(nodes: int = 1, devices: int = 1) -> run.LeptonExecutor:
         )
     )
 
-def prepare_slim_pajama():
-    executor = lepton_executor(nodes=8, devices=1)
-
+def prepare_nemotron_cc():
     # Create a NeMo-Run experiment which runs all sub-steps sequentially
-    with run.Experiment("slim-pajama-data-prep") as exp:
-        exp.add(run.Partial(download, "/nemo-workspace/data"), name="download", executor=executor)
+    with run.Experiment("nemotron-cc-data-prep") as exp:
+        # Data download only needs a single device
+        executor = lepton_executor(nodes=1, devices=1)
+        exp.add(run.Script("/nemo_run/code/data_prep/download.sh", args=["/nemo-workspace/data"]), name="download", executor=executor)
+        # Extract, concat, and preprocess benefit from multiple nodes
+        executor = lepton_executor(nodes=8, devices=1)
         exp.add(run.Partial(extract, "/nemo-workspace/data"), name="extract", executor=executor)
         exp.add(run.Script("/nemo_run/code/data_prep/concat.sh", args=["/nemo-workspace/data"]), name="concat", executor=executor)
-        exp.add(run.Partial(preprocess, "/nemo-workspace/data"), name="preprocess", executor=executor)
+        # Preprocessing requires more system memory to prepare the large files
+        executor = lepton_executor(nodes=4, devices=8)
+        exp.add(run.Partial(prepare, "/nemo-workspace/data"), name="preprocess", executor=executor)
 
         # Launch the experiment on the cluster
         exp.run(sequential=True)
 
 if __name__ == "__main__":
-    prepare_slim_pajama()
-
+    prepare_nemotron_cc()
--- nemo/e2e/nemotronh-pretrain-lepton.py
+++ nemo/e2e/nemotronh-pretrain-lepton.py
@@ -17,62 +17,69 @@ import os
 import nemo_run as run
 
 from nemo.collections import llm
-from nemo.collections.common.tokenizers.huggingface.auto_tokenizer import AutoTokenizer
+from nemo.collections.common.tokenizers import AutoTokenizer
 from nemo.collections.llm.gpt.data.pre_training import PreTrainingDataModule
 from nemo.collections.llm.recipes.log.default import default_log, wandb_logger
 from nemo.collections.llm.recipes.optim.adam import distributed_fused_adam_with_cosine_annealing
-from nemo.utils.exp_manager import TimingCallback
+
 from scripts.convert import convert_checkpoint
 
 
-def configure_recipe(nodes: int = 1, gpus_per_node: int = 2, dir=None, name="nemo"):
-    paths = [os.path.join(data_dir, f"llama-slim-pajama-{num}_text_document") for num in range(30)]
-    tokenizer = run.Config(AutoTokenizer, pretrained_model_name="meta-llama/Llama-3.1-8B")
+def configure_recipe(
+        nodes: int = 1,
+        gpus_per_node: int = 2,
+        dir: str = "/nemo-workspace/nemotronh_8b",
+        name: str = "nemotronh_8b"
+    ):
+    paths = [os.path.join("/nemo-workspace/data/", f"nemotron-cc-{num}_text_document") for num in range(465)]
+    tokenizer = run.Config(AutoTokenizer, pretrained_model_name="nvidia/Nemotron-H-8B-Base-8K")
 
     data=run.Config(
         PreTrainingDataModule,
         paths=paths,
-        seq_length=8192,  # Use a sequence length or context window of 8K tokens
-        global_batch_size=512,  # Batch size of 512
+        seq_length=8192,
+        global_batch_size=768,
         micro_batch_size=1,
         tokenizer=tokenizer
     )
 
     wandb = wandb_logger(
-        project="llama-3.1",  # Specify the Weights & Biases project name
-        name="llama-3.1-8b"  # Specify the name of the training run to be displayed on Weights & Biases
+        project="nemotronh",
+        name="nemotronh-8b"
     )
 
     recipe = run.Partial(
-        llm.pretrain,  # Specify that we want to use the Pre-train method
-        model=llm.llama31_8b.model(),  # Use the existing Llama 3.1-8B model config default settings
-        trainer=llm.llama31_8b.trainer(
+        llm.pretrain,
+        model=llm.nemotronh_8b.model(),
+        trainer=llm.nemotronh_8b.trainer(
+            dir=dir, # Path to store checkpoints
             num_nodes=nodes,
             num_gpus_per_node=gpus_per_node,
-            max_steps=150000,  # Train for 150,000 steps - equal to 150,000 * batch size (512) * sequence length (8192) = 629B tokens
-            callbacks=[run.Config(TimingCallback)],
+            val_check_interval=2000,
+            limit_test_batches=10,
+            limit_val_batches=10,
+            ckpt_async_save=True,
+            max_steps=160000,
         ),
         data=data,
-        optim=distributed_fused_adam_with_cosine_annealing(max_lr=3e-4),
+        optim=distributed_fused_adam_with_cosine_annealing(max_lr=8e-4),
         log=default_log(dir=dir, name=name, wandb_logger=wandb),
     )
 
-    recipe.trainer.val_check_interval = 2000  # Run evaluation and save a checkpoint every 2,000 steps
-    recipe.trainer.strategy.tensor_model_parallel_size = 4  # Set the Tensor Parallelism size to 4
     return recipe
 
 def lepton_executor(nodes: int = 1, devices: int = 1) -> run.LeptonExecutor:
     mounts = [
         {
             "path": "/nemo-workspace",  # Directory to mount from the remote filesystem
-            "mount_path": "/nemo-workspace"  # Where to mount the directory in pods
+            "mount_path": "/nemo-workspace",  # Where to mount the directory in pods
             "from": "local:nfs"  # (Optional) Which remote storage resource to mount
         }
     ]
 
     return run.LeptonExecutor(
         resource_shape="gpu.8xh100-80gb",  # Replace with the resource shape for the node group
-        container_image="nvcr.io/nvidia/nemo:25.02",  # Which container to deploy
+        container_image="nvcr.io/nvidia/nemo:25.04",  # Which container to deploy
         nemo_run_dir="/nemo-workspace/nemo-run",  # Specify the NeMo-Run directory to copy experiments to in the remote filesystem
         mounts=mounts,  # Which directories to mount from the remote filesystem
         node_group="xxxxx",  # Replace with the name of the node group available in the cluster
@@ -92,17 +99,14 @@ def lepton_executor(nodes: int = 1, devices: int = 1) -> run.LeptonExecutor:
     )
 
 def run_pretraining():
-    recipe = configure_recipe(nodes=8, gpus_per_node=8, dir="/nemo-workspace/llama-3.1-8b", name="llama-3.1-8b")
+    recipe = configure_recipe(nodes=8, gpus_per_node=8)
     executor = lepton_executor(nodes=recipe.trainer.num_nodes, devices=recipe.trainer.devices)
 
     run.run(recipe, executor=executor)
 
-    # Re-initialize the executor as only a single GPU is needed for conversion
     executor = lepton_executor(nodes=1, devices=1)
-    export_ckpt = convert_checkpoint(dir="/nemo-workspace/llama-3.1-8b")
+    run.run(run.Partial(convert_checkpoint, "/nemo-workspace/nemotronh_8b"), name="convert-model", executor=executor)
 
-    run.run(run.Partial(convert_checkpoint, "/nemo-workspace/llama-3.1-8b"), name="convert-model", executor=executor)
 
 if __name__ == "__main__":
     run_pretraining()
-
